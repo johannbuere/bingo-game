262eaae5e8fad314416873c9e99dc393ed45d12a
Johann Buere <johannbuere@gmail.com>
2024-11-24 11:47:47 +0800

    Removed old files and added modified game files

---
+++ src/BingoCaller.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BingoCaller {
+    private ArrayList<String> calls;
+    private int currentCallIndex;
+    private int maxCalls;
+
+    public BingoCaller(int maxCalls) {
+        this.maxCalls = maxCalls;
+        calls = new ArrayList<>();
+        char[] letters = {'B', 'I', 'N', 'G', 'O'};
+
+        for (int i = 0; i < letters.length; i++) {
+            int start = i * 15 + 1;
+            int end = start + 15;
+            for (int num = start; num < end; num++) {
+                calls.add(letters[i] + String.valueOf(num));
+            }
+        }
+        Collections.shuffle(calls);
+        currentCallIndex = -1;
+    }
+
+
+    public String getNextCall() {
+        if (currentCallIndex < maxCalls - 1 && currentCallIndex < calls.size() - 1) {
+            currentCallIndex++;
+            String nextCall = calls.get(currentCallIndex);
+            return nextCall;
+        }
+        return null;
+    }
+
+
+    public boolean hasCallsLeft() {
+        return currentCallIndex < maxCalls - 1;
+    }
+
+    public ArrayList<String> getCalledNumbers() {
+        return new ArrayList<>(calls.subList(0, currentCallIndex + 1));
+    }
+}
---
+++ src/BingoMainMenu.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+
+public class BingoMainMenu {
+    public void launch() {
+        JFrame frame = new JFrame("Bingo Game - Main Menu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        JButton startGame = new JButton("Start Game");
+        JButton exitGame = new JButton("Exit");
+
+        startGame.addActionListener(e -> {
+            frame.dispose();
+            showGameSettings();
+        });
+
+        exitGame.addActionListener(e -> System.exit(0));
+
+        panel.add(startGame);
+        panel.add(exitGame);
+        frame.add(panel);
+
+        frame.setSize(300, 150);
+        frame.setVisible(true);
+    }
+
+    private void showGameSettings() {
+        JFrame frame = new JFrame("Bingo Game - Settings");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        JLabel callsLabel = new JLabel("Number of Calls: ");
+        JLabel cardsLabel = new JLabel("Number of Cards: ");
+
+        JTextField callsField = new JTextField(5);
+        JTextField cardsField = new JTextField(5);
+
+        JButton startGame = new JButton("Start Game");
+
+        startGame.addActionListener(e -> {
+            try {
+                int numCalls = Integer.parseInt(callsField.getText());
+                int numCards = Integer.parseInt(cardsField.getText());
+                frame.dispose();
+                new BingoUI(numCalls, numCards).launch();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(frame, "Please enter valid numbers!");
+            }
+        });
+
+        panel.add(callsLabel);
+        panel.add(callsField);
+        panel.add(cardsLabel);
+        panel.add(cardsField);
+        panel.add(startGame);
+
+        frame.add(panel);
+        frame.setSize(400, 200);
+        frame.setVisible(true);
+    }
+}
